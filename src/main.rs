8d222f411d48a6bc94f87dec2c86cf590a9819a5
Stefan Sydow <s.sydow@heinlein-video.de>
2021-06-16 15:26:33 +0200

    Ensure that error handling will log all errors
    Signed-off-by: Stefan Sydow <s.sydow@heinlein-video.de>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::settings::Logging;
+use crate::settings::{Logging, Settings};
 use actix_cors::Cors;
 use actix_web::http::{header, Method};
 use actix_web::web::Data;
@@ -24,18 +24,26 @@ extern crate diesel;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let settings = settings::load_settings()?;
+    let settings = settings::load_settings().context("Failed to load settings from file")?;
     setup_logging(&settings.logging)?;
     log::debug!("Starting K3K Controller with settings {:?}", settings);
     // Run database migration
-    db::migrations::start_migration(&settings.database).await?;
+    if let Err(e) = db::migrations::start_migration(&settings.database).await {
+        log::error!(target: "db", "Failed to migrate database: {}", e);
+        return Err(e);
+    }
 
+    service(settings).await.map_err(|e| {
+        log::error!("Crashed with error: {}", e);
+        e
+    })
+}
+
+async fn service(settings: Settings) -> Result<()> {
     let db_ctx = Data::new(
-        DbInterface::connect(settings.database)
-            .context("Failed to initialize Database connection")?,
+        DbInterface::connect(settings.database).context("Failed to connect to database")?,
     );
 
-    // Discover OIDC Provider
     let oidc_ctx = Data::new(
         OidcContext::from_config(settings.oidc)
             .await
@@ -53,6 +61,7 @@ async fn main() -> Result<()> {
     // Start internal HTTP Server
     let internal_db_ctx = db_ctx.clone();
     let internal_oidc_ctx = oidc_ctx.clone();
+    let internal_address = (Ipv6Addr::UNSPECIFIED, settings.http.internal_port);
     let internal_http_server = HttpServer::new(move || {
         App::new()
             .app_data(internal_db_ctx.clone())
@@ -62,6 +71,7 @@ async fn main() -> Result<()> {
 
     // Start external HTTP Server
     let cors = settings.http.cors;
+    let ext_address = (Ipv6Addr::UNSPECIFIED, settings.http.port);
     let ext_http_server = HttpServer::new(move || {
         let cors = setup_cors(&cors);
 
@@ -75,27 +85,32 @@ async fn main() -> Result<()> {
     });
 
     let (ext_http_server, internal_http_server) = if let Some(tls) = settings.http.tls {
-        let config = setup_rustls(tls)?;
-
+        let config = setup_rustls(tls).context("Failed to setup TLS context")?;
         (
-            ext_http_server
-                .bind_rustls((Ipv6Addr::UNSPECIFIED, settings.http.port), config.clone())?,
-            internal_http_server
-                .bind_rustls((Ipv6Addr::UNSPECIFIED, settings.http.internal_port), config)?,
+            ext_http_server.bind_rustls(ext_address, config.clone()),
+            internal_http_server.bind_rustls(internal_address, config),
         )
     } else {
         (
-            ext_http_server.bind((Ipv6Addr::UNSPECIFIED, settings.http.port))?,
-            internal_http_server.bind((Ipv6Addr::UNSPECIFIED, settings.http.internal_port))?,
+            ext_http_server.bind(ext_address),
+            internal_http_server.bind(internal_address),
         )
     };
 
+    let ext_http_server = ext_http_server
+        .with_context(|| format!("Failed to bind to external port {}", ext_address.1))?;
+
+    let internal_http_server = internal_http_server
+        .with_context(|| format!("Failed to bind to internal port {}", internal_address.1))?;
+
+    log::info!("Startup finished");
+
     match select(ext_http_server.run(), internal_http_server.run()).await {
         Either::Left((external_res, _external_server)) => {
-            external_res.expect("External server error: {}")
+            external_res.context("External server crashed")?
         }
         Either::Right((internal_res, _internal_server)) => {
-            internal_res.expect("Internal server error: {}")
+            internal_res.context("Internal server crashed")?
         }
     };
 
@@ -153,7 +168,8 @@ fn setup_logging(logging: &Logging) -> Result<()> {
             let log_file = std::fs::OpenOptions::new()
                 .create(true)
                 .append(true)
-                .open(path)?;
+                .open(path)
+                .context("Failed to create or open the logging file")?;
 
             logger.chain(log_file)
         }
@@ -166,11 +182,23 @@ fn setup_logging(logging: &Logging) -> Result<()> {
 fn setup_rustls(tls: settings::HttpTls) -> Result<rustls::ServerConfig> {
     let mut config = rustls::ServerConfig::new(rustls::NoClientAuth::new());
 
-    let certs = certs(&mut BufReader::new(File::open(tls.certificate)?))
-        .map_err(|_| anyhow!("Failed to read certificate file"))?;
+    let cert_file = File::open(&tls.certificate).with_context(|| {
+        format!(
+            "Failed to open certificate file {}",
+            &tls.certificate.to_string_lossy()
+        )
+    })?;
+    let certs =
+        certs(&mut BufReader::new(cert_file)).map_err(|_| anyhow!("Invalid certificate"))?;
 
-    let mut key = rsa_private_keys(&mut BufReader::new(File::open(tls.private_key)?))
-        .map_err(|_| anyhow!("Failed to read pkcs8 private key file"))?;
+    let priv_key_file = File::open(&tls.private_key).with_context(|| {
+        format!(
+            "Failed to open pkcs8 private key file {}",
+            &tls.private_key.to_string_lossy()
+        )
+    })?;
+    let mut key = rsa_private_keys(&mut BufReader::new(priv_key_file))
+        .map_err(|_| anyhow!("Invalid pkcs8 private key"))?;
 
     config.set_single_cert(certs, key.remove(0))?;
 
