7a8c4bd8d105504f21ff5516c8b3401c842ad1dc
Marvin Ankert <m.ankert@heinlein-video.de>
2021-07-22 14:53:03 +0200

    Refactor ws_service to improve readablility

--- src/api/signaling/ws/http.rs
+++ src/api/signaling/ws/http.rs
@@ -4,7 +4,7 @@ use super::runner::Runner;
 use super::ParticipantId;
 use super::SignalingModule;
 use crate::api::v1::rooms::{Ticket, TicketData};
-use crate::api::v1::DefaultApiError;
+use crate::api::v1::{ApiError, DefaultApiError};
 use crate::db::rooms::Room;
 use crate::db::users::User;
 use crate::db::DbInterface;
@@ -99,106 +99,16 @@ async fn ws_service(
     rabbit_mq_channel: lapin::Channel,
     protocols: &'static [&'static str],
     modules: Rc<[Box<dyn ModuleBuilder>]>,
-    req: HttpRequest,
+    request: HttpRequest,
     stream: web::Payload,
 ) -> actix_web::Result<HttpResponse> {
-    let mut protocol = None;
-    let mut ticket = None;
+    let (ticket, protocol) = read_request_header(&request, protocols)?;
 
-    if let Some(value) = req
-        .headers()
-        .get(header::SEC_WEBSOCKET_PROTOCOL)
-        .and_then(|v| v.to_str().ok())
-    {
-        let values = value.split(',').map(str::trim);
+    let ticket_data = get_ticket_data_from_redis(&mut redis_conn, &ticket).await?;
 
-        for value in values {
-            if value.starts_with("ticket#") {
-                let (_, tmp_ticket) = value.split_once("#").unwrap();
-                ticket = Some(Ticket {
-                    ticket: tmp_ticket.into(),
-                });
-                continue;
-            } else if protocol.is_some() {
-                continue;
-            } else if let Some(value) = protocols.iter().find(|&&v| v == value) {
-                protocol = Some(value);
-            }
-        }
-    }
+    let (user, room) = get_user_and_room_from_ticket(db_ctx.clone(), ticket_data).await?;
 
-    let protocol = match protocol {
-        Some(protocol) => protocol,
-        None => {
-            log::debug!("Rejecting websocket request, missing valid protocol");
-            return Err(DefaultApiError::BadRequest("Missing protocol".into()).into());
-        }
-    };
-
-    let ticket = match ticket {
-        Some(ticket) => match ticket.validate() {
-            Ok(_) => ticket,
-            Err(e) => {
-                log::warn!("Ticket validation failed for {:?}, {}", ticket, e);
-
-                return Err(DefaultApiError::auth_bearer_invalid_token(
-                    "ticket invalid",
-                    "Please request a new ticket from /v1/rooms/<room_uuid>/start".into(),
-                )
-                .into());
-            }
-        },
-        None => {
-            log::debug!("Rejecting websocket request, missing ticket");
-            return Err(DefaultApiError::auth_bearer_invalid_request(
-                "missing ticket",
-                "Please request a ticket from /v1/rooms/<room_uuid>/start".into(),
-            )
-            .into());
-        }
-    };
-
-    // GETDEL available since redis 6.2.0, missing direct support by redis crate
-    let ticket_data: Option<TicketData> = redis::cmd("GETDEL")
-        .arg(&ticket)
-        .query_async(&mut redis_conn)
-        .await
-        .map_err(|e| {
-            log::warn!("Unable to get ticket data in redis: {}", e);
-            DefaultApiError::Internal
-        })?;
-
-    let ticket_data = ticket_data.ok_or_else(|| {
-        DefaultApiError::auth_bearer_invalid_token(
-            "ticket invalid or expired",
-            "Please request a new ticket from /v1/rooms/<room_uuid>/start".into(),
-        )
-    })?;
-
-    let cloned_db_ctx = db_ctx.clone();
-
-    let (user, room) = web::block(move || -> Result<(User, Room), DefaultApiError> {
-        let user = cloned_db_ctx
-            .get_user_by_uuid(&ticket_data.user)
-            .map_err(DefaultApiError::from)?;
-
-        let user = user.ok_or(DefaultApiError::Internal)?;
-
-        let room = cloned_db_ctx
-            .get_room_by_uuid(&ticket_data.room)
-            .map_err(DefaultApiError::from)?;
-
-        let room = room.ok_or(DefaultApiError::NotFound)?;
-
-        Ok((user, room))
-    })
-    .await
-    .map_err(|e| {
-        log::error!("BlockingError on ws_service - {}", e);
-        DefaultApiError::Internal
-    })??;
-
-    let mut response = ws::handshake(&req)?;
+    let mut response = ws::handshake(&request)?;
 
     let (adapter, actix_stream) = ActixTungsteniteAdapter::from_actix_payload(stream);
     let websocket = WebSocketStream::from_raw_socket(adapter, Role::Server, None).await;
@@ -242,4 +152,118 @@ async fn ws_service(
     ));
 
     Ok(response.streaming(actix_stream))
+}
+
+fn read_request_header(
+    request: &HttpRequest,
+    allowed_protocols: &'static [&'static str],
+) -> Result<(Ticket, &'static str), ApiError> {
+    let mut protocol = None;
+    let mut ticket = None;
+
+    // read the SEC_WEBSOCKET_PROTOCOL header
+    if let Some(value) = request
+        .headers()
+        .get(header::SEC_WEBSOCKET_PROTOCOL)
+        .and_then(|v| v.to_str().ok())
+    {
+        let values = value.split(',').map(str::trim);
+
+        for value in values {
+            if value.starts_with("ticket#") {
+                let (_, tmp_ticket) = value.split_once("#").unwrap();
+                ticket = Some(Ticket {
+                    ticket: tmp_ticket.into(),
+                });
+                continue;
+            } else if protocol.is_some() {
+                continue;
+            } else if let Some(value) = allowed_protocols.iter().find(|&&v| v == value) {
+                protocol = Some(value);
+            }
+        }
+    }
+
+    // look if valid protocol exists
+    let protocol = match protocol {
+        Some(protocol) => protocol,
+        None => {
+            log::debug!("Rejecting websocket request, missing valid protocol");
+            return Err(DefaultApiError::BadRequest("Missing protocol".into()));
+        }
+    };
+
+    // look if ticket exists
+    let ticket = match ticket {
+        Some(ticket) => match ticket.validate() {
+            Ok(_) => ticket,
+            Err(e) => {
+                log::warn!("Ticket validation failed for {:?}, {}", ticket, e);
+
+                return Err(DefaultApiError::auth_bearer_invalid_token(
+                    "ticket invalid",
+                    "Please request a new ticket from /v1/rooms/<room_uuid>/start".into(),
+                ));
+            }
+        },
+        None => {
+            log::debug!("Rejecting websocket request, missing ticket");
+            return Err(DefaultApiError::auth_bearer_invalid_request(
+                "missing ticket",
+                "Please request a ticket from /v1/rooms/<room_uuid>/start".into(),
+            ));
+        }
+    };
+
+    Ok((ticket, protocol))
+}
+
+async fn get_ticket_data_from_redis(
+    redis_conn: &mut ConnectionManager,
+    ticket: &Ticket,
+) -> Result<TicketData, ApiError> {
+    // GETDEL available since redis 6.2.0, missing direct support by redis crate
+    let ticket_data: Option<TicketData> = redis::cmd("GETDEL")
+        .arg(ticket)
+        .query_async(redis_conn)
+        .await
+        .map_err(|e| {
+            log::warn!("Unable to get ticket data in redis: {}", e);
+            DefaultApiError::Internal
+        })?;
+
+    let ticket_data = ticket_data.ok_or_else(|| {
+        DefaultApiError::auth_bearer_invalid_token(
+            "ticket invalid or expired",
+            "Please request a new ticket from /v1/rooms/<room_uuid>/start".into(),
+        )
+    })?;
+
+    Ok(ticket_data)
+}
+
+async fn get_user_and_room_from_ticket(
+    db_ctx: Data<DbInterface>,
+    ticket_data: TicketData,
+) -> Result<(User, Room), ApiError> {
+    web::block(move || -> Result<(User, Room), DefaultApiError> {
+        let user = db_ctx
+            .get_user_by_uuid(&ticket_data.user)
+            .map_err(DefaultApiError::from)?;
+
+        let user = user.ok_or(DefaultApiError::Internal)?;
+
+        let room = db_ctx
+            .get_room_by_uuid(&ticket_data.room)
+            .map_err(DefaultApiError::from)?;
+
+        let room = room.ok_or(DefaultApiError::NotFound)?;
+
+        Ok((user, room))
+    })
+    .await
+    .map_err(|e| {
+        log::error!("BlockingError on ws_service - {}", e);
+        DefaultApiError::Internal
+    })?
 }
