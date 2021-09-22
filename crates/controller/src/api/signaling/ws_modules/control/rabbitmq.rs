228b307e5ff684d1104b4f168feabe699411fce9
Konstantin Baltruschat <k.baltruschat@heinlein-video.de>
2021-09-22 14:20:22 +0200

    Fix last minute changes made to breakout rooms
    control::rabbitmq::current_room_exchange_name returned the wrong
    rabbitmq exchange name because it was missing the `room=` prefix.
    
    Also let ee-chat add queue binding using control::rabbitmq::current_room_exchange_name

--- crates/controller/src/api/signaling/ws_modules/control/rabbitmq.rs
+++ crates/controller/src/api/signaling/ws_modules/control/rabbitmq.rs
@@ -13,16 +13,16 @@ pub enum Message {
 
 /// Returns the name of the RabbitMQ topic exchange used to communicate inside a room.
 ///
-/// Note that this exchange is used to communicate across breakout-room boundries and
-/// should only be used in special circumstances where that behaviour is intended.
+/// Note that this exchange is used to communicate across breakout-room boundaries and
+/// should only be used in special circumstances where that behavior is intended.
 pub fn room_exchange_name(room: RoomId) -> String {
     format!("k3k-signaling.room={}", room.into_inner())
 }
 
 /// Returns the name of the RabbitMQ topic exchange used inside the current room.
-/// This exchange should be used when writing behaviour constrained to a single room
+/// This exchange should be used when writing behavior constrained to a single room
 pub fn current_room_exchange_name(room: SignalingRoomId) -> String {
-    format!("k3k-signaling.{}", room)
+    format!("k3k-signaling.room={}", room)
 }
 
 /// Returns the routing-key/topic used to send a message to the given participant
