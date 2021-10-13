87506894b18fb42fa9f8495783715e79879dfd6d
Marvin Ankert <m.ankert@heinlein-video.de>
2021-10-13 21:33:28 +0200

    Add user input validation to legalvote

--- crates/legal-vote/src/incoming.rs
+++ crates/legal-vote/src/incoming.rs
@@ -2,6 +2,7 @@ use super::VoteOption;
 use controller::db::legal_votes::VoteId;
 use controller::prelude::*;
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 /// An incoming message issued by an participant
 #[derive(Debug, Deserialize)]
@@ -17,20 +18,35 @@ pub enum Message {
     Vote(VoteMessage),
 }
 
+impl Validate for Message {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        match self {
+            Message::Start(user_parameters) => user_parameters.validate(),
+            Message::Stop(_) => Ok(()),
+            Message::Cancel(cancel) => cancel.validate(),
+            Message::Vote(_) => Ok(()),
+        }
+    }
+}
+
 /// The users parameters to start a new vote
-#[derive(Debug, Clone, Serialize, PartialEq, Eq, Deserialize)]
+#[derive(Debug, Clone, Serialize, PartialEq, Eq, Deserialize, Validate)]
 pub struct UserParameters {
     /// The name of the vote
+    #[validate(length(max = 150))]
     pub name: String,
     /// The topic that will be voted on
+    #[validate(length(max = 500))]
     pub topic: String,
     /// List of participants that are allowed to cast a vote
+    #[validate(length(min = 1))]
     pub allowed_participants: Vec<ParticipantId>,
     /// Indicates that the `Abstain` vote option is enabled
     pub enable_abstain: bool,
     /// The vote will automatically stop when every participant voted
     pub auto_stop: bool,
     /// The vote will stop when the duration (in seconds) has passed
+    #[validate(range(min = 5))]
     pub duration: Option<u64>,
 }
 
@@ -45,11 +61,12 @@ impl_to_redis_args_se!(UserParameters);
 impl_from_redis_value_de!(UserParameters);
 
 /// Cancel a vote
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Validate)]
 pub struct Cancel {
     /// The vote id of the targeted vote
     pub vote_id: VoteId,
     /// The reason for the cancel
+    #[validate(length(max = 255))]
     pub reason: String,
 }
 
@@ -198,6 +215,70 @@ mod test {
             assert_eq!(option, VoteOption::Abstain);
         } else {
             panic!()
+        }
+    }
+
+    #[test]
+    fn invalid_start_message() {
+        let string_151 = "X".repeat(151);
+        let string_501 = "X".repeat(501);
+
+        let json_str = format!(
+            r#"
+        {{
+            "action": "start",
+            "name": "{}",
+            "topic": "{}",
+            "allowed_participants": [],
+            "enable_abstain": false,
+            "auto_stop": false,
+            "duration": 4 
+        }}
+        "#,
+            string_151, string_501
+        );
+
+        let start: Message = serde_json::from_str(&json_str).unwrap();
+
+        if let Err(validation_errors) = start.validate().into() {
+            let errors = validation_errors.errors();
+
+            assert!(errors.contains_key("name"));
+            assert!(errors.contains_key("topic"));
+            assert!(errors.contains_key("allowed_participants"));
+            assert!(errors.contains_key("duration"));
+
+            assert_eq!(errors.len(), 4);
+        } else {
+            panic!("Expected validation errors");
+        }
+    }
+
+    #[test]
+    fn invalid_cancel_message() {
+        let string_256 = "X".repeat(256);
+
+        let json_str = format!(
+            r#"
+        {{
+            "action": "cancel",
+            "vote_id": "00000000-0000-0000-0000-000000000000",
+            "reason": "{}"
+        }}
+        "#,
+            string_256
+        );
+
+        let cancel: Message = serde_json::from_str(&json_str).unwrap();
+
+        if let Err(validation_errors) = cancel.validate().into() {
+            let errors = validation_errors.errors();
+
+            assert!(errors.contains_key("reason"));
+
+            assert_eq!(errors.len(), 1);
+        } else {
+            panic!("Expected validation errors");
         }
     }
 }
