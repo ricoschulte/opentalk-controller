d24d562518c19602f74ab32392c8fa3058e85367
Konstantin Baltruschat <k.baltruschat@heinlein-video.de>
2022-01-10 18:32:48 +0100

    controller/signaling: add `participantion_kind` to the control data for all participants
    This allows the frontend to allow special handling for certain participants.
    
    As an example SIP users will (at least for now) not display any video and must be
    treated differently when displaying their display name (phone number).
    
    Guests may be treated differently from logged-in users in some scenarios (e.g. legalvote)

--- crates/controller/src/api/signaling/ws_modules/control/mod.rs
+++ crates/controller/src/api/signaling/ws_modules/control/mod.rs
@@ -2,7 +2,7 @@
 //!
 //! Actual control 'module' code can be found inside `crate::api::signaling::ws::runner`
 use crate::Timestamp;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 pub mod incoming;
 pub mod outgoing;
@@ -15,8 +15,20 @@ pub const NAMESPACE: &str = "control";
 #[derive(Debug, Serialize)]
 pub struct ControlData {
     pub display_name: String,
+    pub participation_kind: ParticipationKind,
     pub hand_is_up: bool,
     pub joined_at: Timestamp,
     pub left_at: Option<Timestamp>,
     pub hand_updated_at: Timestamp,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ParticipationKind {
+    User,
+    Guest,
+    Sip,
+}
+
+impl_to_redis_args_se!(ParticipationKind);
+impl_from_redis_value_de!(ParticipationKind);
