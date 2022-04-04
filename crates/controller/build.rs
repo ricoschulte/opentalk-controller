e39cc729fc75563d9d1a5181652f7393809505d6
Rudi Floren <r.floren@heinlein-video.de>
2022-04-04 19:51:14 +0200

    Disable git vergen for release builds
    When no .git directory is present, e.g. in release tar balls, we now
    disable the git feature of vergen. In this case the binary will print
    out N/A.

--- crates/controller/build.rs
+++ crates/controller/build.rs
@@ -17,6 +17,32 @@ use anyhow::Result;
 use vergen::{vergen, Config};
 
 fn main() -> Result<()> {
+    let config = if !git_at_cd_or_above()? {
+        // Setup your config without git enabled
+        let mut config = Config::default();
+        *config.git_mut().enabled_mut() = false;
+        config
+    } else {
+        // Setup your config with git enabled
+        Config::default()
+    };
+
     // Generate the default 'cargo:' instruction output
-    vergen(Config::default())
+    vergen(config)
+}
+
+fn git_at_cd_or_above() -> Result<bool> {
+    let current_dir = std::env::current_dir()?;
+    let mut parents = vec![];
+    let mut path = &*current_dir.canonicalize()?;
+    while let Some(parent) = path.parent() {
+        parents.push(parent);
+        path = parent;
+    }
+    for parent in parents.into_iter().rev() {
+        if parent.join(".git").exists() {
+            return Ok(true);
+        }
+    }
+    Ok(false)
 }
