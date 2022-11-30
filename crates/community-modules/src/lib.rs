ff398a8dcb97eeeedf13b4f8885ec2a9150e5cf6
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2022-11-30 09:37:37 +0100

    Remove source code that should not be in the community edition
    See: #257

--- crates/community-modules/src/lib.rs
+++ crates/community-modules/src/lib.rs
@@ -3,9 +3,7 @@ use controller::Controller;
 
 pub async fn register(controller: &mut Controller) -> Result<()> {
     chat::register(controller);
-    automod::register(controller);
     janus_media::register(controller).await?;
-    legal_vote::register(controller);
     polls::register(controller);
     protocol::register(controller);
     timer::register(controller);
