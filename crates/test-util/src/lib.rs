a908abdf4796bea677f91638fc8ae82b0aa64898
Werner Rabl <werner.rabl@opentalk.eu>
2023-01-02 18:59:04 +0100

    feature gates for test-utils

--- crates/test-util/src/lib.rs
+++ crates/test-util/src/lib.rs
@@ -1,95 +1,18 @@
 //! Test utility functions for use with the module tester and the controller
-use crate::database::DatabaseContext;
-use anyhow::Result;
-use controller::prelude::anyhow::Context;
-use controller::prelude::*;
-use controller_shared::ParticipantId;
-use db_storage::rooms::RoomId;
-use kustos::Authz;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::sync::broadcast::Sender;
-use uuid::Uuid;
-
 pub use ::serde_json;
 pub use pretty_assertions::assert_eq;
 
+#[cfg(feature = "controller")]
+pub use common::{TestContext, TestUser, ROOM_ID, USERS, USER_1, USER_2};
+
+#[cfg(feature = "controller")]
 pub mod common;
-pub mod database;
+
+#[cfg(feature = "controller")]
 pub mod redis;
 
-#[derive(Debug)]
-pub struct TestUser {
-    pub n: u32,
-    pub participant_id: ParticipantId,
-    pub name: &'static str,
-}
-
-pub const ROOM_ID: RoomId = RoomId::from(Uuid::from_u128(2000));
-
-pub const USER_1: TestUser = TestUser {
-    n: 1,
-    participant_id: ParticipantId::new_test(1),
-    name: "user1",
-};
-
-pub const USER_2: TestUser = TestUser {
-    n: 2,
-    participant_id: ParticipantId::new_test(2),
-    name: "user2",
-};
-
-pub const USERS: [TestUser; 2] = [USER_1, USER_2];
-
-/// The [`TestContext`] provides access to redis & postgres for tests
-pub struct TestContext {
-    pub db_ctx: DatabaseContext,
-    pub redis_conn: RedisConnection,
-    pub authz: Arc<Authz>,
-    pub shutdown: Sender<()>,
-}
-
-impl TestContext {
-    /// Creates a new [`TestContext`]
-    pub async fn new() -> Self {
-        let _ = setup_logging();
-
-        let db_ctx = database::DatabaseContext::new(true).await;
-
-        let (shutdown, _) = tokio::sync::broadcast::channel(10);
-
-        let (enforcer, _) = kustos::Authz::new_with_autoload(
-            db_ctx.db.clone(),
-            shutdown.subscribe(),
-            Duration::from_secs(5),
-        )
-        .await
-        .unwrap();
-
-        TestContext {
-            db_ctx,
-            redis_conn: redis::setup().await,
-            authz: Arc::new(enforcer),
-            shutdown,
-        }
-    }
-}
-
-pub fn setup_logging() -> Result<()> {
-    fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{}][{}] {}",
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .level(log::LevelFilter::Info)
-        .chain(std::io::stdout())
-        .apply()
-        .context("Failed to setup logging utility")
-}
+#[cfg(feature = "database")]
+pub mod database;
 
 /// Helper macro to compare a `[Serialize]` implementor with a JSON literal
 ///
